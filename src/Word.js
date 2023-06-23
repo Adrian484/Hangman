62395d30ada1a52f29ee1662a6b6e5303526ac5f
Adrian <Adrian.Davis484@outlook.com>
2023-06-23 11:36:13 +0930

    Random word and keyboard works

--- src/Word.js
+++ src/Word.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 const Word = ({ word, guessedLetters }) => {
-  const displayWord = word
-    .split('')
-    .map((letter, index) =>
-      guessedLetters.includes(letter) ? letter : '_'
-    )
-    .join(' ');
+  const getLetterDisplay = (letter) => {
+    if (guessedLetters.includes(letter)) {
+      return letter;
+    } else {
+      return '_';
+    }
+  };
 
-  return <div>{displayWord}</div>;
+  return (
+    <div>
+      {word.split('').map((letter, index) => (
+        <span key={index} className="letter">
+          {getLetterDisplay(letter)}
+          {' '}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default Word;
