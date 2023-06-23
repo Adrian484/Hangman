3d0e43b9247a29b7c680b5aa35fcfe8424cc8f5e
Adrian <Adrian.Davis484@outlook.com>
2023-06-23 19:07:06 +0930

    I now have two different randomised coloured backgrounds, unintentional but in my opinion, it's a better feature. Working on a button for the outer layer...

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hangman from './Hangman';
 
+const App = () => {
+  const generateRandomColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const color = `rgb(${red}, ${green}, ${blue})`;
+    document.body.style.backgroundColor = color;
+  };
+
+  // Generate random background color on initial page load
+  React.useEffect(() => {
+    generateRandomColor();
+  }, []);
+
+  return <Hangman generateRandomColor={generateRandomColor} />;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
