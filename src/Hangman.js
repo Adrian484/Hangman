c3103e7593827b597ba301bf62cae8da53855106
Adrian <Adrian.Davis484@outlook.com>
2023-06-26 12:52:19 +0930

    Keyboard layout is now like a standard keyboard and is centered to page

--- src/Hangman.js
+++ src/Hangman.js
@@ -131,72 +131,22 @@ function Hangman() {
         ))}
       </div>
       <div className="keyboard">
-        {Array.from(Array(26), (_, index) => {
-          const letter = String.fromCharCode(65 + index);
-          return (
-            <button
-              key={letter}
-              onClick={() => handleButtonClick(letter)}
-              disabled={disabledLetters.includes(letter.toUpperCase())}
-            >
-              {letter}
-            </button>
-          );
-        })}
+        {['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM'].map((row, rowIndex) => (
+          <div key={rowIndex} className="keyboard-row">
+            {row.split('').map((letter, index) => (
+              <button
+                key={index}
+                onClick={() => handleButtonClick(letter)}
+                disabled={disabledLetters.includes(letter.toUpperCase())}
+                className="button-qwerty"
+              >
+                {letter}
+              </button>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
 }
-
 export default Hangman;
-
-
-
-
-
-// import React, { useEffect, useState } from 'react';
-// import Keyboard from './Keyboard';
-// import Word from './Word';
-// import RandomWord from './RandomWord';
-
-// function Hangman() {
-//   const [randomWord, setRandomWord] = useState('');
-//   const [guessedLetters, setGuessedLetters] = useState([]);
-//   const [disabledLetters, setDisabledLetters] = useState([]);
-
-//   useEffect(() => {
-//     fetch('https://random-word-api.herokuapp.com/word')
-//       .then(response => response.json())
-//       .then(data => {
-//         const word = data[0];
-//         setRandomWord(word);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching random word:', error);
-//       });
-//   }, []);
-
-//   const handleLetterClick = (letter) => {
-//     setGuessedLetters([...guessedLetters, letter]);
-//     setDisabledLetters([...disabledLetters, letter]);
-//   };
-
-//   return (
-//     <div>
-//       <h1>Hangman Game</h1>
-//       <RandomWord word={randomWord} />
-//       <Word word={randomWord} guessedLetters={guessedLetters} />
-//       <Keyboard
-//         letters={['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']}
-//         disabledLetters={disabledLetters}
-//         onClick={handleLetterClick}
-//       />
-//     </div>
-
-
-
-    
-//   );
-// }
-
-// export default Hangman;
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-const Keyboard = ({ letters, disabledLetters, onClick }) => {
-  // Define the rows of the keyboard
-  const keyboardRows = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM'];
+const Keyboard = ({ disabledLetters, onClick }) => {
+  const keyboardRows = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+  ];
 
   return (
     <div className="keyboard">
       {keyboardRows.map((row, rowIndex) => (
         <div key={rowIndex} className="keyboard-row">
-          {row.split('').map((letter, index) => (
+          {row.map((letter, index) => (
             <button
               key={index}
               onClick={() => onClick(letter)}
               disabled={disabledLetters.includes(letter)}
+              style={{ width: '40px', height: '40px' }}
             >
               {letter}
             </button>
